1400c699ba31728273bed6c0a92ed5b862e47e8e
laurendiperna <laurendiperna@gmail.com>
2016-12-19 17:48:32 -0800

    changed the pysimplib namd to pysimplib_LD so the package could be accepted on pypi, update mytest.py to have a unit test for the module's mymath.py, added setup files for pypi in the projects root directory, updated version number in setup.py to .2

--- mytest.py
+++ mytest.py
@@ -1,5 +1,16 @@
-def greeting():
-    print('hello');
+import unittest
 
-def dragon_greeting():
-    print("RAAR! (with flames)");
+import pysimplib_LD.mymath as mymod
+
+class SimplisticTest(unittest.TestCase):
+
+    def test_add(self):
+    	x = mymod.myadd(10,20)
+        self.assertEqual(x, 30)
+
+    def test_sub(self):
+    	x = mymod.mysub(10,20)
+    	self.assertEqual(x, -10)
+
+if __name__ == '__main__':
+    unittest.main()
